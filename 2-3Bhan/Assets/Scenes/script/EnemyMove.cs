2f749a6579ee135b874b16e8e3a8125991b4d0ee
KBC12005185\kbc12005185 <kana5665>
2022-01-14 14:55:52 +0900

    K5

--- 2-3Bhan/Assets/Scenes/script/EnemyMove.cs
+++ 2-3Bhan/Assets/Scenes/script/EnemyMove.cs
@@ -1,5 +1,4 @@
-﻿// Patrol.cs
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
 
@@ -16,9 +15,6 @@ public class EnemyMove : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
 
-        // autoBraking を無効にすると、目標地点の間を継続的に移動します
-        //(つまり、エージェントは目標地点に近づいても
-        // 速度をおとしません)
         agent.autoBraking = false;
 
         GotoNextPoint();
@@ -27,23 +23,17 @@ public class EnemyMove : MonoBehaviour
 
     void GotoNextPoint()
     {
-        // 地点がなにも設定されていないときに返します
         if (points.Length == 0)
             return;
 
-        // エージェントが現在設定された目標地点に行くように設定します
         agent.destination = points[destPoint].position;
 
-        // 配列内の次の位置を目標地点に設定し、
-        // 必要ならば出発地点にもどります
         destPoint = (destPoint + 1) % points.Length;
     }
 
 
     void Update()
     {
-        // エージェントが現目標地点に近づいてきたら、
-        // 次の目標地点を選択します
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
     }
