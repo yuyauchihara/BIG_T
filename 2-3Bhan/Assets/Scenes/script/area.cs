e5c703ff73f1e8393e7cbc3db1b8e86f73968d59
DESKTOP-P6Q5SBR\NEC-PCuser <eri-nakamura-20f@stu.kbc.ac.jp>
2022-01-18 13:20:00 +0900

    競合怖いのでスクリプトだけぷしゅります（各休息エリアのistriggerにチェック入れてほしいです）areaスクリプトは各休息エリアobjに、gbスクリプトはプレイヤーobjにぶち込んでほしいです

---
+++ 2-3Bhan/Assets/Scenes/script/area.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class area : MonoBehaviour
+{
+    public static int cnt=0;
+    public static bool i = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+
+	void OnTriggerStay(Collider other)//エリア内に接している間ずっと呼び出される
+	{
+		
+		if (other.gameObject.CompareTag("Player"))
+        { 
+			Debug.Log("内");
+            i = true;
+		}
+	}
+}
+
