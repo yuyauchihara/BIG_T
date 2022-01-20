76ca80c8cbec07ec66892530e6dd606b167dd63a
KBC12005021\kbc12005021 <taichi-tomoyose-20f@stu.kbc.ac.jp>
2022-01-20 10:54:16 +0900

    menu iranaiyatu

---
+++ 2-3Bhan/Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class UIManager : MonoBehaviour
+{
+
+    //３つのPanelを格納する変数
+    //インスペクターウィンドウからゲームオブジェクトを設定する
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject xrHubPanel;
+    [SerializeField] GameObject unityPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //BackToMenuメソッドを呼び出す
+        BackToMenu();
+    }
+
+
+    //MenuPanelでXR-HubButtonが押されたときの処理
+    //XR-HubPanelをアクティブにする
+    public void SelectXrHubDescription()
+    {
+        menuPanel.SetActive(false);
+        xrHubPanel.SetActive(true);
+    }
+
+
+    //MenuPanelでUnityButtonが押されたときの処理
+    //UnityPanelをアクティブにする
+    public void SelectUnityDescription()
+    {
+        menuPanel.SetActive(false);
+        unityPanel.SetActive(true);
+    }
+
+
+    //2つのDescriptionPanelでBackButtonが押されたときの処理
+    //MenuPanelをアクティブにする
+    public void BackToMenu()
+    {
+        menuPanel.SetActive(true);
+        xrHubPanel.SetActive(false);
+        unityPanel.SetActive(false);
+    }
+}
---
+++ 2-3Bhan/Assets/Scripts/menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+public class menu : MonoBehaviour
+{
+	Button cube;
+	Button sphere;
+	Button cylinder;
+
+	void Start()
+	{
+		// ボタンコンポーネントの取得
+		cube = GameObject.Find("/Canvas/Button1").GetComponent<Button>();
+		sphere = GameObject.Find("/Canvas/Button2").GetComponent<Button>();
+		cylinder = GameObject.Find("/Canvas/Button3").GetComponent<Button>();
+
+		// 最初に選択状態にしたいボタンの設定
+		cube.Select();
+	}
+}
