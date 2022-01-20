368be93fa5e7c5bd517dc4efe7b1f58ec3da5dad
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2022-01-20 10:51:59 +0900

    chase用SE

---
+++ 2-3Bhan/Assets/script/ChaseSE.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseSE : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !audioSource.isPlaying)
+        {
+            //音(sound1)を鳴らす
+            audioSource.PlayOneShot(sound1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        audioSource.Stop();
+    }
+}
