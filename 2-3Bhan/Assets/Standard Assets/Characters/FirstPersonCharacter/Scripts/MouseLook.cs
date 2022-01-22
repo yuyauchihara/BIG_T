17ff1f2971c0b9d1fa3220f050dc2be644f0db64
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-01-22 15:02:03 +0900

    Pause中視点移動させないスクリプト

--- 2-3Bhan/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ 2-3Bhan/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -65,35 +65,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
         }
 
-        //public void UpdateCursorLock()
-        //{
-        //    //if the user set "lockCursor" we check & properly lock the cursos
-        //    if (lockCursor)
-        //        InternalLockUpdate();
-        //}
-
-        //private void InternalLockUpdate()
-        //{
-        //    if(Input.GetKeyUp(KeyCode.Escape))
-        //    {
-        //        m_cursorIsLocked = false;
-        //    }
-        //    else if(Input.GetMouseButtonUp(0))
-        //    {
-        //        m_cursorIsLocked = true;
-        //    }
-
-        //    if (m_cursorIsLocked)
-        //    {
-        //        Cursor.lockState = CursorLockMode.Locked;
-        //        Cursor.visible = false;
-        //    }
-        //    else if (!m_cursorIsLocked)
-        //    {
-        //        Cursor.lockState = CursorLockMode.None;
-        //        Cursor.visible = true;
-        //    }
-        //}
 
         Quaternion ClampRotationAroundXAxis(Quaternion q)
         {
--- 2-3Bhan/Assets/script/PouseChoice.cs
+++ 2-3Bhan/Assets/script/PouseChoice.cs
@@ -12,18 +12,9 @@ public class PouseChoice : MonoBehaviour
     void OnEnable()
     {
         button2.GetComponent<Button>().Select();
-        //button = GameObject.Find("menu/Panel/re").GetComponent<Button>();
         //ボタンが選択された状態になる
         button.GetComponent<Button>().Select();
-        Debug.Log("aaa");
 
     }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown("joystick button 10"))
-    //    {
-    //        a.position += new Vector3(1f + 1, 0, 0);
-    //    }
-    //}
 }
