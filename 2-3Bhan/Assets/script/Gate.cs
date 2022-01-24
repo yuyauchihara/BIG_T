68816790faf8dfc1058f63221c01408e7f5a109b
ariasmiyagi <arias-miyagi-20f@stu.kbc.ac.jp>
2022-01-24 09:59:05 +0900

    goal tuika

---
+++ 2-3Bhan/Assets/script/Gate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Gate : MonoBehaviour
+{
+    int score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (this.score >= 300)
+        {
+            Destroy(gameObject);
+            
+        }
+       
+
+    }
+}
