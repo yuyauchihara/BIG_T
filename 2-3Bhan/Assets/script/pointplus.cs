1b07f3e87acea47bddcea6f907152e0494ce6e19
DESKTOP-P6Q5SBR\NEC-PCuser <eri-nakamura-20f@stu.kbc.ac.jp>
2022-01-24 15:19:44 +0900

    ポイントの表示、アニメーションの速度を若干はやめた

--- 2-3Bhan/Assets/script/pointplus.cs
+++ 2-3Bhan/Assets/script/pointplus.cs
@@ -8,10 +8,13 @@ public class pointplus : MonoBehaviour
     public Text pointText;
     public Animator anim;
 
+ 
+    float cTime = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        cTime = 0f;
     }
 
     // Update is called once per frame
@@ -20,12 +23,19 @@ public class pointplus : MonoBehaviour
 
         if (area.i == true)
         {
+            cTime += Time.deltaTime;
+
             if (getPoint.tmp != 0 && getPoint.clear == false)
             {
+               
+
+                anim.SetFloat("Speed", cTime);
                 anim.SetTrigger("AnimationSendEvent");
                 pointText.text = string.Format("+{0}", getPoint.tmp);
+                cTime = 0f;
+               
             }
-
+            
 
         }
         else
