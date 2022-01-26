c6a70bf51f9175b206d350c6d36006e40573457f
KBC12005185\kbc12005185 <kana5665>
2022-01-26 16:13:08 +0900

    GameOver

--- 2-3Bhan/Assets/Scenes/script/GameOver.cs
+++ 2-3Bhan/Assets/Scenes/script/GameOver.cs
@@ -2,25 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    public GameObject FPSController;
-    public Canvas GameOverText;
-    FirstPersonController controller;
 
     void Start()
     {
-        controller = FPSController.GetComponent<FirstPersonController>();
-        GameOverText.enabled = false;
-        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -28,9 +23,7 @@ public class GameOver : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("触れた");
-            GameOverText.enabled = true;
-            controller.enabled = false;
-            Time.timeScale = 0f;
+            SceneManager.LoadScene("GameOverScene");
         }
     }
 }
