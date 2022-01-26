53a405b4a0fe06d4b05dc1851d1833b3aca29cfb
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2022-01-26 14:14:45 +0900

    ゲートが開く音の追加

---
+++ 2-3Bhan/Assets/script/GateSE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSE : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    int Gflag = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (getPoint.cPoint == 1000)
+        {
+            if (Gflag == 0)
+            {
+                //音(sound1)を鳴らす
+                audioSource.PlayOneShot(sound1);
+                Gflag = 1;
+            }
+
+        }
+    }
+}
