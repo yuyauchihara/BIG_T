6a20ffd6f806b4e40e6df49e84e26e3415636e16
KBC12005185\kbc12005185 <kana5665>
2022-01-26 16:01:38 +0900

    GameOver

---
+++ 2-3Bhan/Assets/script/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("触れた");
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
