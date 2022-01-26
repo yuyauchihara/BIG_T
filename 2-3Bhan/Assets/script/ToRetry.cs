e0374be64ec420d3d15d2eaae3348c18df5be490
KBC12005021\kbc12005021 <taichi-tomoyose-20f@stu.kbc.ac.jp>
2022-01-26 20:55:23 +0900

    GameからRetryへシーン切り替え

---
+++ 2-3Bhan/Assets/script/ToRetry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ToRetry : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ClearCoroutine());
+    }
+
+    IEnumerator ClearCoroutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene("Retry");
+    }
+}
